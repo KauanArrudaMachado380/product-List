f3c0cddbaa37da00fd545e8b540cd70bfb1f9f44
KauanArrudaMachado380 <152728989+KauanArrudaMachado380@users.noreply.github.com>
2024-11-01 14:39:54 -0300

    mexendo na lógica do confirm

--- js/confirm.js
+++ js/confirm.js
@@ -24,8 +24,6 @@ const cardConfirm = {
 
         items.forEach((order) => {
             this.cardsDesserts(order);
-            console.log(order);
-            
         });
 
         const orderTotal = document.createElement('p');
@@ -40,33 +38,56 @@ const cardConfirm = {
         button.classList.add('card_confirm-button');
         button.textContent = 'Start New Order';
         button.addEventListener('click', () => {
-            window.location.href = './index.html';
+            
         });
 
         confirmDiv.appendChild(img);
         confirmDiv.appendChild(titule);
         confirmDiv.appendChild(text);
+
+        ordersDiv.appendChild(orderTotal);
+        ordersDiv.appendChild(totalPrice);
+
         confirmDiv.appendChild(ordersDiv);
-        confirmDiv.appendChild(orderTotal);
-        confirmDiv.appendChild(totalPrice);
+        confirmDiv.appendChild(button);
     },
 
-    cardsDesserts (order){ 
+    cardsDesserts (order){
         
+        const container = document.querySelector('.card_confirm-orders');
+
+        const elementDiv = document.createElement('div');
+        elementDiv.classList.add('card_confirm__item');
+
         const img = document.createElement('img');
         img.classList.add('card_confirm-img');
 
         const titule = document.createElement('h2');
         titule.classList.add('card_confirm-titule');
-        
+        titule.textContent = `${order.children[0].textContent}`;
+
         const quantity = document.createElement('p');
         quantity.classList.add('card_confirm-quantity');
+        quantity.textContent = `${order.children[1].textContent}`;
 
         const price = document.createElement('p');
         price.classList.add('card_confirm-price');
+        price.textContent = `$${order.children[2].textContent.replace('$', '')}`;
 
         const totalPrice = document.createElement('p');
         totalPrice.classList.add('card_confirm-totalPrice');
+        totalPrice.textContent = `$${order.children[3].textContent.replace('$', '')}`;
+
+        elementDiv.appendChild(img);
+        elementDiv.appendChild(titule);
+        elementDiv.appendChild(quantity);
+        elementDiv.appendChild(price);
+        elementDiv.appendChild(totalPrice);
+
+        console.log(elementDiv);
+        
+
+        container.appendChild(elementDiv);
     }
 }
 
